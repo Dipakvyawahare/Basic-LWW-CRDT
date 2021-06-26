5b07b95d9992fb3b98be5b2864a6fa7a9555ca30
Dipak Vyawahare <dipakvyawahare@gmail.com>
2021-06-26 19:59:49 +0530

    UI and Unit Test added

--- CloudNotesTests/LWWSetTests.swift
+++ CloudNotesTests/LWWSetTests.swift
@@ -9,25 +9,50 @@ import XCTest
 @testable import CloudNotes
 
 class LWWSetTests: XCTestCase {
+    let subjectUnderTest = LWWSet<String>()
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testAdd() {
+        let value = "1"
+        subjectUnderTest.add(value)
+        assert(subjectUnderTest.values().contains(value), "Add Test failed")
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testRemoveEmpty() {
+        let value = "1"
+        subjectUnderTest.remove(value)
+        assert(subjectUnderTest.values().contains(value) == false, "Remove Test failed")
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testRemoveFilled() {
+        let value = "1"
+        subjectUnderTest.add(value)
+        subjectUnderTest.remove(value)
+        assert(subjectUnderTest.values().contains(value) == false, "Remove Test failed")
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testRemoveNotExist() {
+        let value = "1"
+        let value2 = "2"
+        subjectUnderTest.add(value)
+        subjectUnderTest.remove(value2)
+        assert(subjectUnderTest.values().contains(value2) == false, "Remove Test failed")
     }
 
+    func testValues() {
+        let value1 = "1"
+        let value2 = "2"
+        subjectUnderTest.add(value1)
+        subjectUnderTest.add(value2)
+        assert(subjectUnderTest.values().count == 2, "Values Test failed")
+    }
+
+    func testMerge() {
+        let otherSet = LWWSet<String>()
+        let value1 = "1"
+        let value2 = "2"
+        otherSet.add(value1)
+        subjectUnderTest.add(value2)
+        let mergedSet = subjectUnderTest.merge(otherSet)
+        assert(mergedSet.values().count == 2, "Merge Test failed")
+    }
 }
